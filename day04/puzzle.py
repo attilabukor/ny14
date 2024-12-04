40f346e1be0e7b0bb5c9c8a5d9553baceec96c9b
Attila Bukor <attila@bukor.me>
2024-12-04 08:00:59 +0100

    Refactor day 4 pt 1

--- day04/puzzle.py
+++ day04/puzzle.py
@@ -2,6 +2,51 @@
 
 import fileinput, sys, re
 
+def part1(matrix):
+    count = 0
+    w = len(matrix[0])
+    h = len(matrix)
+
+    for r in range(0, h):
+        for c in range(0, w):
+            if matrix[r][c] != "X":
+                continue
+
+            if c < w - 3:
+                if (matrix[r][c+1] == "M" and matrix[r][c+2] == "A" and
+                    matrix[r][c+3] == "S"):
+                    count += 1
+            if c >= 3:
+                if (matrix[r][c-1] == "M" and matrix[r][c-2] == "A" and
+                    matrix[r][c-3] == "S"):
+                    count += 1
+            if r < h - 3:
+                if (matrix[r+1][c] == "M" and matrix[r+2][c] == "A" and
+                    matrix[r+3][c] == "S"):
+                    count += 1
+            if r >= 3:
+                if (matrix[r-1][c] == "M" and matrix[r-2][c] == "A" and
+                    matrix[r-3][c] == "S"):
+                    count += 1
+            if c < w - 3 and r < h - 3:
+                if (matrix[r+1][c+1] == "M" and matrix[r+2][c+2] == "A" and
+                    matrix[r+3][c+3] == "S"):
+                    count += 1
+            if c >= 3 and r >= 3:
+                if (matrix[r-1][c-1] == "M" and matrix[r-2][c-2] == "A" and
+                    matrix[r-3][c-3] == "S"):
+                    count += 1
+            if c < w - 3 and r >= 3:
+                if (matrix[r-1][c+1] == "M" and matrix[r-2][c+2] == "A" and
+                    matrix[r-3][c+3] == "S"):
+                    count += 1
+            if c >= 3 and r < w - 3:
+                if (matrix[r+1][c-1] == "M" and matrix[r+2][c-2] == "A" and
+                    matrix[r+3][c-3] == "S"):
+                    count += 1
+
+    return count
+
 matrix = []
 for l in fileinput.input(sys.argv[1:]):
     row = []
@@ -10,47 +55,4 @@ for l in fileinput.input(sys.argv[1:]):
             row.append(c)
     matrix.append(row)
 
-count = 0
-
-w = len(matrix[0])
-h = len(matrix)
-
-for r in range(0, h):
-    for c in range(0, w):
-        if matrix[r][c] != "X":
-            continue
-
-        if c < w - 3:
-            if (matrix[r][c+1] == "M" and matrix[r][c+2] == "A" and
-                matrix[r][c+3] == "S"):
-                count += 1
-        if c >= 3:
-            if (matrix[r][c-1] == "M" and matrix[r][c-2] == "A" and
-                matrix[r][c-3] == "S"):
-                count += 1
-        if r < h - 3:
-            if (matrix[r+1][c] == "M" and matrix[r+2][c] == "A" and
-                matrix[r+3][c] == "S"):
-                count += 1
-        if r >= 3:
-            if (matrix[r-1][c] == "M" and matrix[r-2][c] == "A" and
-                matrix[r-3][c] == "S"):
-                count += 1
-        if c < w - 3 and r < h - 3:
-            if (matrix[r+1][c+1] == "M" and matrix[r+2][c+2] == "A" and
-                matrix[r+3][c+3] == "S"):
-                count += 1
-        if c >= 3 and r >= 3:
-            if (matrix[r-1][c-1] == "M" and matrix[r-2][c-2] == "A" and
-                matrix[r-3][c-3] == "S"):
-                count += 1
-        if c < w - 3 and r >= 3:
-            if (matrix[r-1][c+1] == "M" and matrix[r-2][c+2] == "A" and
-                matrix[r-3][c+3] == "S"):
-                count += 1
-        if c >= 3 and r < w - 3:
-            if (matrix[r+1][c-1] == "M" and matrix[r+2][c-2] == "A" and
-                matrix[r+3][c-3] == "S"):
-                count += 1
-
-print(count)
+print(part1(matrix))
