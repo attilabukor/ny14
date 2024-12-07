560b7db17c0c6b07a1b6d38b5c00dfceab720390
Attila Bukor <abukor@cloudera.com>
2024-12-07 08:14:57 +0100

    Day 7 pt 2

--- day07/puzzle.py
+++ day07/puzzle.py
@@ -2,13 +2,15 @@
 
 import fileinput
 
-def check(val, nums):
+def check(val, nums, concat):
     res = [nums[0]]
     for n in nums[1:]:
         partial = []
         for r in res:
             partial.append(r * n)
             partial.append(r + n)
+            if concat:
+                partial.append(int(str(r) + str(n)))
         res = partial
 
     if val in res:
@@ -17,6 +19,7 @@ def check(val, nums):
         return False
 
 part1 = 0
+part2 = 0
 for l in fileinput.input():
     val, rest = l.strip().split(":")
     val = int(val)
@@ -25,7 +28,10 @@ for l in fileinput.input():
         n = int(n)
         nums.append(int(n))
 
-    if check(val, nums):
+    if check(val, nums, False):
         part1 += val
+    if check(val, nums, True):
+        part2 += val
 
 print(part1)
+print(part2)
