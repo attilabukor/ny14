3138009bc317cff410d770aa2860454c1a79dff6
Attila Bukor <abukor@cloudera.com>
2024-12-10 09:11:30 +0100

    Day 10 pt 1

---
+++ day10/puzzle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import fileinput, sys
+
+def draw(matrix):
+    for l in matrix:
+        for c in l:
+            if c == None:
+                print(".", end="")
+            else:
+                print(c, end="")
+        print()
+
+def walk(matrix, start, found_peaks, prev=None):
+    x = start[0]
+    y = start[1]
+    start_val = matrix[x][y]
+    if start_val == 9:
+        if start in found_peaks:
+            return 0
+        else:
+            found_peaks.append(start)
+            return 1
+
+
+    sum = 0
+
+    if x > 0 and matrix[x-1][y] == start_val + 1 and prev != (x-1,y):
+        sum += walk(matrix, (x-1,y), found_peaks, start)
+    if x + 1< len(matrix) and matrix[x+1][y] == start_val + 1 and prev != (x+1,y):
+        sum += walk(matrix, (x+1,y), found_peaks, start)
+    if y > 0 and matrix[x][y-1] == start_val + 1 and prev != (x,y-1):
+        sum += walk(matrix, (x,y-1), found_peaks, start)
+    if y + 1 < len(matrix) and matrix[x][y+1] == start_val + 1 and prev != (x,y+1):
+        sum += walk(matrix, (x,y+1), found_peaks, start)
+
+    return sum
+
+island = []
+trailheads = []
+peaks = []
+for i, l in enumerate(fileinput.input()):
+    line = []
+    for j, c in enumerate(l.strip()):
+        if c == '.':
+            c = None
+        else:
+            c = int(c)
+        line.append(c)
+        if c == 0:
+            trailheads.append((i,j))
+        elif c == 9:
+            peaks.append((i,j))
+    island.append(line)
+
+sum = 0
+for t in trailheads:
+    score = walk(island, t, [])
+    sum += score
+
+print(sum)
+
