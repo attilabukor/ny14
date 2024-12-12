f297a0a80df06b5f7b69e28fdf06765e8bd7c1e8
Attila Bukor <abukor@cloudera.com>
2024-12-12 18:54:16 +0100

    Day 12 pt 2

--- day12/puzzle.py
+++ day12/puzzle.py
@@ -5,7 +5,10 @@ import fileinput
 def draw(matrix):
     for l in matrix:
         for c in l:
-            print(c, end="")
+            if c == None:
+                print(".", end="")
+            else:
+                print(c, end="")
         print()
 
 def load():
@@ -60,8 +63,38 @@ def perimeter(region):
 
     return perimeter
 
+def sides(region):
+    sides = 0
+    for p in region:
+        x = p[0]
+        y = p[1]
+
+        # Outer corners clockwise from top-left corner
+        if (x-1,y) not in region and (x,y-1) not in region:
+            sides += 1
+        if (x,y-1) not in region and (x+1,y) not in region:
+            sides += 1
+        if (x+1,y) not in region and (x,y+1) not in region:
+            sides += 1
+        if (x-1,y) not in region and (x,y+1) not in region:
+            sides += 1
+
+        # Inner corners clockwise from top-left corner
+        if (x+1,y+1) not in region and (x+1,y) in region and (x,y+1) in region:
+            sides += 1
+        if (x-1,y-1) not in region and (x-1,y) in region and (x,y-1) in region:
+            sides += 1
+        if (x-1,y+1) not in region and (x-1,y) in region and (x,y+1) in region:
+            sides += 1
+        if (x+1,y-1) not in region and (x+1,y) in region and (x,y-1) in region:
+            sides += 1
+
+    return sides
+
 all_walked = []
 price = 0
+disc_price = 0
+
 for x, row in enumerate(farm):
     for y, name in enumerate(row):
         if (x,y) in all_walked:
@@ -69,7 +102,9 @@ for x, row in enumerate(farm):
         
         region = get_region(farm, (x,y), [])
         #print(name, "area", area(region), "perimeter", perimeter(region))
+        #print(name, "area", area(region), "sides", sides(region))
         price += area(region) * perimeter(region)
+        disc_price += area(region) * sides(region)
         all_walked.extend(region)
 
-print(price)
+print(price, disc_price)
